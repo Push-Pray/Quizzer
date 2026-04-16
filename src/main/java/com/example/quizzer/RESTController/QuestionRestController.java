08d2af1a9487414a994a6fea4a49f92c3bb64ede
Mehedi Hasan <mehedihasanhridoy20@gmail.com>
2026-04-16 15:27:24 +0300

    user story 6. add API endpoint to fetch all questions for a specific quiz

--- src/main/java/com/example/quizzer/RESTController/QuestionRestController.java
+++ src/main/java/com/example/quizzer/RESTController/QuestionRestController.java
@@ -1,10 +1,14 @@
 package com.example.quizzer.RESTController;
 
 import com.example.quizzer.DTO.QuestionDTO;
+import com.example.quizzer.DTO.QuestionInfoDTO;
 import com.example.quizzer.DTO.QuizzInfoDTO;
+import com.example.quizzer.service.QuestionService; 
 import com.example.quizzer.service.QuizzService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List; 
 
 @RestController
 @RequestMapping("/api")
@@ -12,20 +16,27 @@ import org.springframework.web.bind.annotation.*;
 public class QuestionRestController {
 
     private final QuizzService quizzService;
+    private final QuestionService questionService;
 
-    public QuestionRestController(QuizzService quizzService) {
-
+    public QuestionRestController(QuizzService quizzService, QuestionService questionService) {
         this.quizzService = quizzService;
+        this.questionService = questionService;
     }
 
     @PostMapping("/quizz/{id}/question")
-    public ResponseEntity <Object> createQuestion(@PathVariable Long id, @RequestBody QuestionDTO questionDTO){
+    public ResponseEntity<Object> createQuestion(@PathVariable Long id, @RequestBody QuestionDTO questionDTO){
         try {
             QuizzInfoDTO quizzInfoDTO = quizzService.addQuestion(id, questionDTO);
             return ResponseEntity.ok(questionDTO);
         }catch (RuntimeException e){
-
             return ResponseEntity.badRequest().body(e.getMessage());
         }
+    }
+
+    @GetMapping("/course/{quizzId}/questions")
+    public ResponseEntity<List<QuestionInfoDTO>> getQuestionsForQuizz(@PathVariable Long quizzId) {
+        List<QuestionInfoDTO> questions = quizzService.getQuestionsByQuizzId(quizzId);
+        
+        return ResponseEntity.ok(questions);
     }
 }
