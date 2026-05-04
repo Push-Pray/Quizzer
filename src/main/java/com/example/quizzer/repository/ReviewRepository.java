7dc084e97a022b72b3154d336ab86e66b39766b5
Jaime <67060224+Mineja2017@users.noreply.github.com>
2026-05-04 12:08:25 +0300

    Add function to search based on quizzId

--- src/main/java/com/example/quizzer/repository/ReviewRepository.java
+++ src/main/java/com/example/quizzer/repository/ReviewRepository.java
@@ -4,7 +4,10 @@ import com.example.quizzer.model.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public interface ReviewRepository extends JpaRepository <Review, Long> {
 
+    public List<Review> findAllByQuizzId(Long quizzId);
 }
